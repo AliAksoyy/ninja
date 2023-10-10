5a8a3db9ecab49a75732b008bef5a86d6a32ec30
AliAksoyy <alifeyza4017@gmail.com>
2023-10-10 15:10:45 +0300

    added

---
+++ Modern Javascript/main.js
@@ -0,0 +1,9 @@
+// string
+
+let email = "mario@thenetninja.co.uk";
+let firstName = "Brandon";
+let lastName = "Sanderson";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName.length);
+console.log(fullName.toUpperCase());

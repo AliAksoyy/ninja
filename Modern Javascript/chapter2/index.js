88fa9a25259e02b7e1596dcd8cdbf2a4b3b3ab46
AliAksoyy <alifeyza4017@gmail.com>
2023-10-10 23:15:44 +0300

    added

--- Modern Javascript/chapter2/index.js
+++ Modern Javascript/chapter2/index.js
@@ -1,5 +1,7 @@
 // for loop
 
+const { textSpanIntersectsWith } = require("typescript");
+
 for (let i = 0; i < 5; i++) {
   console.log(i);
 }
@@ -28,3 +30,86 @@ do {
     i++;
   }
 } while (i < 5);
+
+// if statements
+
+const age2 = 25;
+
+if (age2 > 20) {
+  console.log("you are over 20 yeas old");
+}
+const ninjas = ["shaun", "ryu", "chun-li", "yoshi"];
+
+if (ninjas.length > 3) {
+  console.log("that's is a lot of ninjas");
+}
+
+const password = "pass";
+if (password.length > 8) {
+  console.log("thas password is long enough");
+} else {
+  console.log("password is not long enough");
+}
+
+let user = false;
+
+if (!user) {
+  console.log("wrong");
+}
+
+let scores1 = [50, 0, 25, 300, 100, 20, 10];
+
+for (let i = 0; i < scores1.length; i++) {
+  if (!scores1[i]) {
+    const index = scores1.indexOf(scores1[i]);
+    console.log(index);
+    if (index !== -1) {
+      scores1.splice(index, 1);
+    }
+  }
+}
+console.log(scores1);
+
+//break and contuine
+
+let scores2 = [50, 0, 25, 100, 20, 10];
+
+for (let i = 0; i < scores2.length; i++) {
+  if (scores2[i] === 0) {
+    continue;
+  }
+  console.log(scores2[i]);
+  if (scores2[i] === 100) {
+    console.log("congrat");
+    break;
+  }
+}
+
+//  swtich statement
+
+const grade = "D";
+
+if (grade === "A") {
+} else if (grade === "B") {
+} else if (grade === "C") {
+} else {
+}
+
+switch (grade) {
+  case "A": {
+    console.log("object");
+    break;
+  }
+  case "B": {
+    break;
+  }
+  case "C": {
+    break;
+  }
+  default: {
+  }
+}
+
+// variables & block scope
+
+
